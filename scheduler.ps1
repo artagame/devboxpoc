d8cf4f7f8581fce849f1a1421d54dd9bae1b6d6e
artagame <44085422+artagame@users.noreply.github.com>
2024-11-05 12:55:57 +1100

    Update scheduler.ps1

--- scheduler.ps1
+++ scheduler.ps1
@@ -12,7 +12,7 @@ $Trigger = $Task.Triggers.Create(9)
 $Trigger.Enabled = $true
 $Action = $Task.Actions.Create(0)
 $Action.Path = "C:\Program Files\PowerShell\7\pwsh.exe"
-$Action.Arguments = "-MTA -Command C:\scripts\installVSCodeExtensionsAndCloneRepo.ps1"
+$Action.Arguments = "-MTA -Command C:\installVSCodeExtensionsAndCloneRepo.ps1"
 $TaskFolder = $ShedService.GetFolder("\")
 $TaskFolder.RegisterTaskDefinition("$($RunAsUserTask)", $Task , 6, "Users", $null, 4)
 Write-Host "Done setting up scheduled tasks"
