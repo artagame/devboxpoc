2be05f789a9c38bb590a97b9d74c1c2ecbba6721
artagame <44085422+artagame@users.noreply.github.com>
2024-11-05 11:16:26 +1100

    Add files via upload

---
+++ installVSCodeExtensionsAndCloneRepo.ps1
@@ -0,0 +1,17 @@
+﻿$vs_code_path = "C:/Program Files/Microsoft VS Code/bin/code.cmd"
+
+# Array of VS Code extensions to install
+$extensions = @(
+    "dbaeumer.vscode-eslint",
+    "esbenp.prettier-vscode",
+    "flowtype.flow-for-vscode",
+    "mgmcdermott.vscode-language-babel",
+    "DevCenter.ms-devbox"
+)
+
+foreach ($extension in $extensions) {
+ Start-Process -FilePath $vs_code_path -ArgumentList "--install-extension $extension" -Wait -NoNewWindow
+}
+
+cd C:\Workspaces
+git clone https://avadevboxpoc@dev.azure.com/avadevboxpoc/Dev%20Box%20POC/_git/To-Do-List-WebApp
---
+++ scheduler.ps1
@@ -0,0 +1,18 @@
+﻿# Task Scheduler for VS Code Extensions
+$ShedService = New-Object -comobject "Schedule.Service"
+$ShedService.Connect()
+$RunAsUserTask = "InstallVSCodeExtensions"
+# Schedule the script to be run in the user context on login
+$Task = $ShedService.NewTask(0)
+$Task.RegistrationInfo.Description = "Visual Studio Code Extensions"
+$Task.Settings.Enabled = $true
+$Task.Settings.AllowDemandStart = $false
+$Task.Principal.RunLevel = 1
+$Trigger = $Task.Triggers.Create(9)
+$Trigger.Enabled = $true
+$Action = $Task.Actions.Create(0)
+$Action.Path = "C:\Program Files\PowerShell\7\pwsh.exe"
+$Action.Arguments = "-MTA -Command C:\scripts\installVSCodeExtensionsAndCloneRepo.ps1"
+$TaskFolder = $ShedService.GetFolder("\")
+$TaskFolder.RegisterTaskDefinition("$($RunAsUserTask)", $Task , 6, "Users", $null, 4)
+Write-Host "Done setting up scheduled tasks"
