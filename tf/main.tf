f0eeec09a07c16d4593a68eff0526bddfebf9721
Abby Artagame <abaigail.c.artagame@avanoso.com>
2024-11-13 15:57:08 +1100

    Fixed error in null resource

--- tf/main.tf
+++ tf/main.tf
@@ -186,7 +186,6 @@ resource "null_resource" "build_image_template" {
       ARM_SUBSCRIPTION_ID = var.subscription_id
     }
     command = <<EOT
-      az login
       az image builder run -n ${var.imageTemplateName} -g ${data.azurerm_resource_group.rg.name} --no-wait
       az image builder wait -n ${var.imageTemplateName} -g ${data.azurerm_resource_group.rg.name} --custom "lastRunStatus.runState!='Running'"
       az image builder show -n ${var.imageTemplateName} -g ${data.azurerm_resource_group.rg.name}
